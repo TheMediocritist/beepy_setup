e1a84bc9c679b212c9d1df34f968d3eb2ee2583a
TheMediocritist <79881777+TheMediocritist@users.noreply.github.com>
2023-05-25 16:22:14 +1000

    update

--- setup.sh
+++ setup.sh
@@ -30,5 +30,8 @@ sudo sed -i ' 1 s/.*/& fbcon=map:10 fbcon=font:VGA8x16/' /boot/cmdline.txt || { 
 # cd ~/beepberry_setup/keyboard
 # ./installer.sh --BBQ20KBD_TRACKPAD_USE BBQ20KBD_TRACKPAD_AS_KEYS --BBQX0KBD_INT BBQX0KBD_USE_INT || { echo "Error: Failed to install keyboard device driver."; exit 1; }
 
+echo "Connecting to i4 bluetooth keyboard..."
+# echo "connect AA:BB:CC:DD:EE:FF \nquit" | bluetoothctl
+
 echo "Rebooting..."
 sudo shutdown -r now || { echo "Error: Failed to reboot."; exit 1; }
